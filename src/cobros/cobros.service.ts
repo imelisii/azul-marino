e0777da2284f834265385bf05f79cab08dde13f4
Nacho <dyrien7@gmail.com>
2025-04-26 00:39:14 -0300

    agregado de funcion PagaParte y PagaPartes

--- src/cobros/cobros.service.ts
+++ src/cobros/cobros.service.ts
@@ -1,29 +1,21 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateCobroDto } from './dto/create-cobro.dto';
 import { UpdateCobroDto } from './dto/update-cobro.dto';
 import { PrismaClient } from 'generated/prisma';
-import { inscripciones } from '../../generated/prisma/index';
-import { WhatsappService } from 'src/whatsapp/whatsapp.service';
+
 
 @Injectable()
 export class CobrosService extends PrismaClient {
 
-
-  constructor( ) {
+  constructor() {
     super()
   }
-
-
 
   async create(createCobroDto: CreateCobroDto) {
     const { socioId, actividadId, metodoPago, aCuentaDe, monto } = createCobroDto;
 
     const actividad = await this.actividades.findUnique({ where: { id: actividadId } })
     const socio = await this.socios.findUnique({ where: { id: socioId } })
-
-
-    if (!actividad) return new BadRequestException(`No existe la actividad con id ${actividadId}`)
-
 
     return this.$transaction(async (prisma) => {
       await prisma.inscripciones.create({
@@ -38,25 +30,92 @@ export class CobrosService extends PrismaClient {
         data: {
           monto,
           medioPago: metodoPago,
-          descripcion: actividad.descripcion ?? '',
+          descripcion: actividad?.descripcion!,
+          tipoMovimiento: 'INGRESO',
+          cuenta: aCuentaDe,
+        }
+      })
+      return `Se ha creado la cobranza de ${monto} para el socio ${socio?.nombre} en la actividad ${actividad?.descripcion}`;
+    })
+  }
+
+  async cobroPartes(createCobranzaDto: CreateCobroDto) {
+    const { actividadId, metodoPago, monto, socioId, aCuentaDe, metodoPago2 } = createCobranzaDto;
+    const actividad = await this.actividades.findUnique({ where: { id: actividadId } })
+    const socio = await this.socios.findUnique({ where: { id: socioId } })
+
+    return this.$transaction(async (prisma) => {
+      await prisma.inscripciones.create({
+        data: {
+          monto,
+          id_actividad: actividadId,
+          id_socio: socioId
+        }
+      })
+
+      await prisma.caja.create({
+        data: {
+          monto,
+          medioPago: metodoPago,
+          descripcion: actividad?.descripcion!,
+          tipoMovimiento: 'INGRESO',
+          cuenta: aCuentaDe,
+        }
+      })
+      await prisma.caja.create({
+        data: {
+          monto,
+          medioPago: metodoPago2!,
+          descripcion: actividad?.descripcion!,
+          tipoMovimiento: 'INGRESO',
+          cuenta: aCuentaDe,
+        }
+      })
+      return `Se ha creado la cobranza de ${monto} para el socio ${socio?.nombre} en la actividad ${actividad?.descripcion}`;
+    })
+
+  }
+
+
+  async cobroPagaParte(createCobroDto: CreateCobroDto) {
+    const { actividadId, metodoPago, monto, socioId, aCuentaDe } = createCobroDto;
+    const actividad = await this.actividades.findUnique({ where: { id: actividadId } })
+    const socio = await this.socios.findUnique({ where: { id: socioId } })
+
+    return this.$transaction(async (prisma) => {
+      await prisma.inscripciones.create({
+        data: {
+          monto,
+          id_actividad: actividadId,
+          id_socio: socioId
+        }
+      })
+
+      await prisma.caja.create({
+        data: {
+          monto,
+          medioPago: metodoPago,
+          descripcion: actividad?.descripcion!,
           tipoMovimiento: 'INGRESO',
           cuenta: aCuentaDe,
         }
       })
 
+      await prisma.saldos.create({
+        data: {
+          id_socio: socioId,
+          descripcion: `Debe parte de cuota de ${actividad?.descripcion}`,
+          monto: Number(actividad?.precio) - monto,
+          medioDePago: metodoPago,
+        }
+      })
 
 
-      //await this.WhatsappService.sendMessage(`Se ha creado la cobranza de ${monto} para el socio ${socio?.nombre} en la actividad ${actividad.descripcion}`, socio?.celular!)
 
-      return `Se ha creado la cobranza de ${monto} para el socio ${socio?.nombre} en la actividad ${actividad.descripcion}`;
-
-      
-
+      return `Se ha creado la cobranza de ${monto} para el socio ${socio?.nombre} en la actividad ${actividad?.descripcion}`;
     })
-
-    
-
   }
+
 
   findAll() {
     return `This action returns all cobros`;
